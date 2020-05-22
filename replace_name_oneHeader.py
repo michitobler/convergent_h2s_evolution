afea4972aaf92f4f7c3a22e98d793cb070b3034f
Michael Tobler <tobler@ksu.edu>
2020-05-22 10:16:05 -0500

    Add files via upload

---
+++ replace_name_oneHeader.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Author: Joanna L. Kelley
+# replace_name.py
+# Replaces name in fasta with only one fasta entry with new name
+
+import sys
+from optparse import  OptionParser
+import random
+import os
+
+
+USAGE = """USAGE: replace_name.py --f <fasta file> --r <text> --o <output filename> """
+
+parser = OptionParser(USAGE)
+parser.add_option("--f",dest="fastaFile",help="name of fasta file to be processed")
+parser.add_option("--o",dest="outFile",help="name of fasta file to be written as outfile")
+parser.add_option("--r",dest="textData",help="text to replace fasta header")
+
+(options,args)=parser.parse_args()
+
+
+if options.fastaFile is None:
+    parser.error('fasta file not given')
+if options.outFile is None:
+    parser.error('output filename not given')
+if options.textData is None:
+    parser.error('text for replacement not given')
+    
+    
+    
+fasta = open(options.fastaFile,'r')
+newname = options.textData
+newfasta = open(options.outFile, 'w')
+count = 1
+
+for line in fasta:
+    if count == 1 and line.startswith('>'):
+    	tofill = '>' + newname + '\n'
+    	newfasta.write(tofill)
+    	newname = newname + str(count)
+    	count += 1	
+    elif count > 1 and line.startswith('>'):
+    	tofill = ''
+    	newfasta.write(tofill)
+    else:
+        newfasta.write(line)
+
+newfasta.write('\n')
+
+fasta.close()
+newfasta.close()
+
